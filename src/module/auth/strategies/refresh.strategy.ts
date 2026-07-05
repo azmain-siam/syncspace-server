79a9e2a005352b7ea5edec1a49a72074070284d8
azmain-siam <siamhossain2700@gmail.com>
2026-07-05 13:46:47 +0600

    ref(auth): refresh strategy

--- src/module/auth/strategies/refresh.strategy.ts
+++ src/module/auth/strategies/refresh.strategy.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
+import * as bcrypt from 'bcrypt';
+import { Request } from 'express';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from 'src/module/prisma/prisma.service';
 import { JwtPayload } from '../interfaces/jwt-payload.interface';
@@ -18,23 +20,41 @@ export class RefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
       secretOrKey: secret,
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: JwtPayload) {
+  async validate(req: Request, payload: JwtPayload) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new UnauthorizedException();
+    }
+    const refreshToken = authHeader.replace('Bearer ', '');
+
     const user = await this.prisma.user.findUnique({
       where: {
         id: payload.sub,
       },
     });
-    if (!user) {
-      throw new Error('User not found');
+
+    if (!user || !user.hashedRefreshToken) {
+      throw new UnauthorizedException();
     }
 
+    const isRefreshTokenMatched = await bcrypt.compare(
+      refreshToken,
+      user.hashedRefreshToken,
+    );
+
+    if (!isRefreshTokenMatched) {
+      throw new UnauthorizedException();
+    }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...safeUser } = user;
-    return safeUser;
+    return {
+      ...safeUser,
+      refreshToken,
+    };
   }
 }
