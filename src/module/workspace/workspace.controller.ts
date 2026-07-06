73adfbf1cd03e700c51404f4c4608cf55a3f92d7
Siam <siam@bdCallings-Mac-mini-7.local>
2026-07-06 20:21:55 +0600

    feat(workspace): create workspace and workspace member

--- src/module/workspace/workspace.controller.ts
+++ src/module/workspace/workspace.controller.ts
@@ -1,12 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CurrentUser } from 'src/common/decorators/get-user.decorator';
+import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import type { User } from 'src/common/interfaces/user.interface';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 import { WorkspaceService } from './workspace.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('workspace')
 export class WorkspaceController {
   constructor(private readonly workspaceService: WorkspaceService) {}
 
-  // @Post()
-  // create(@Body() createWorkspaceDto: CreateWorkspaceDto) {
-  //   return this.workspaceService.create(createWorkspaceDto);
-  // }
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ResponseMessage('Workspace created successfully')
+  create(
+    @Body() createWorkspaceDto: CreateWorkspaceDto,
+    @CurrentUser() user: User,
+  ) {
+    return this.workspaceService.create(createWorkspaceDto, user.id);
+  }
 }
--- src/module/workspace/workspace.service.ts
+++ src/module/workspace/workspace.service.ts
@@ -1,13 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { WorkspaceRole } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateWorkspaceDto } from './dto/create-workspace.dto';
 
 @Injectable()
 export class WorkspaceService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // async create(createWorkspaceDto: CreateWorkspaceDto) {
-  //   return this.prisma.workspace.create({
-  //     data: createWorkspaceDto,
-  //   });
-  // }
+  async create(createWorkspaceDto: CreateWorkspaceDto, userId: string) {
+    return this.prisma.$transaction(async (tx) => {
+      const workspace = await tx.workspace.create({
+        data: {
+          ownerId: userId,
+          name: createWorkspaceDto.name,
+          logo: createWorkspaceDto.logo,
+          slug: createWorkspaceDto.name.toLowerCase().replace(/ /g, '-'),
+        },
+      });
+
+      await tx.workspaceMember.create({
+        data: {
+          workspaceId: workspace.id,
+          userId,
+          role: WorkspaceRole.OWNER,
+        },
+      });
+
+      return workspace;
+    });
+  }
 }
